18164180b51781c98cb2cd82764bf746b824e352
rasaq-ola <olatech@DESKTOP-2QK9NGF>
2024-07-21 05:28:23 +0100

    0x19. C - Stacks, Queues - LIFO, FIFO

--- execute_instruction.c
+++ execute_instruction.c
@@ -1,34 +1,35 @@
 #include "monty.h"
 
+instruction_t instructions[] = {
+    {"push", push},
+    {"pall", pall},
+    {"pint", pint},
+    {"pop", pop},
+    {"swap", swap},
+    {"add", add},
+    {"nop", nop},
+    {NULL, NULL}
+};
+
 /**
- * execute_instruction - Executes a Monty opcode
- * @stack: Double pointer to the stack
- * @opcode: Opcode to execute
- * @line_number: Line number in the Monty bytecode file
+ * execute_instruction - Executes a given opcode
+ * @opcode: The opcode to execute
+ * @stack: The stack to operate on
+ * @line_number: The current line number in the bytecode file
  */
-void execute_instruction(stack_t **stack, char *opcode, unsigned int line_number)
+void execute_instruction(char *opcode, stack_t **stack, unsigned int line_number)
 {
-	int i;
-	instruction_t instructions[] = {
-		{"push", push},
-		{"pall", pall},
-		{"pint", pint},
-		{"pop", pop},
-		{"swap", swap},
-		{"add", add},
-		{"nop", nop},
-		{NULL, NULL}
-	};
+    unsigned int i;
 
-	for (i = 0; instructions[i].opcode != NULL; i++)
-	{
-		if (strcmp(opcode, instructions[i].opcode) == 0)
-		{
-			instructions[i].f(stack, line_number);
-			return;
-		}
-	}
+    for (i = 0; instructions[i].opcode; i++)
+    {
+        if (strcmp(opcode, instructions[i].opcode) == 0)
+        {
+            instructions[i].f(stack, line_number);
+            return;
+        }
+    }
 
-	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-	exit(EXIT_FAILURE);
+    fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+    exit(EXIT_FAILURE);
 }
--- main.c
+++ main.c
@@ -1,46 +1,73 @@
 #include "monty.h"
 
+#define BUFFER_SIZE 1024
+
+void execute_instruction(char *opcode, stack_t **stack, unsigned int line_number);
+char *custom_getline(FILE *file);
+
 /**
- * main - Entry point for the Monty interpreter
- * @argc: Number of arguments
- * @argv: Arguments
- * Return: EXIT_SUCCESS or EXIT_FAILURE
+ * main - Entry point of the Monty ByteCode interpreter
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on error
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	FILE *file;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	unsigned int line_number = 1;
-	stack_t *stack = NULL;
+    FILE *file;
+    char *line = NULL;
+    unsigned int line_number = 0;
+    stack_t *stack = NULL;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
 
-	file = fopen(argv[1], "r");
-	if (file == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+    file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
 
-	while ((read = getline(&line, &len, file)) != -1)
-	{
-		if (line[0] == '\n' || line[0] == '#')
-		{
-			line_number++;
-			continue;
-		}
-		execute_instruction(&stack, line, line_number);
-		line_number++;
-	}
+    while ((line = custom_getline(file)) != NULL)
+    {
+        char *opcode;
 
-	free(line);
-	fclose(file);
-	free_stack(stack);
-	return (EXIT_SUCCESS);
-} 
+        line_number++;
+        opcode = strtok(line, " \t\n");
+        if (opcode && opcode[0] != '#') /* Ignore comments and blank lines */
+        {
+            execute_instruction(opcode, &stack, line_number);
+        }
+        free(line);
+    }
+
+    free_stack(stack);
+    fclose(file);
+    return (EXIT_SUCCESS);
+}
+
+/**
+ * custom_getline - Custom implementation of getline for C89 compatibility
+ * @file: File pointer
+ * Return: Pointer to the line read, or NULL on failure
+ */
+char *custom_getline(FILE *file)
+{
+    char *line = malloc(BUFFER_SIZE);
+    if (line == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (fgets(line, BUFFER_SIZE, file) == NULL)
+    {
+        free(line);
+        return NULL;
+    }
+
+    return line;
+}
