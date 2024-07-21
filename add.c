689969bb5edc5fe44250ac59df990da434867a9e
rasaq-ola <olatech@DESKTOP-2QK9NGF>
2024-07-21 05:37:43 +0100

    0x19. C - Stacks, Queues - LIFO, FIFO

--- add.c
+++ add.c
@@ -8,13 +8,11 @@
 void add(stack_t **stack, unsigned int line_number)
 {
 	int sum;
-	
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	
 	sum = (*stack)->n + (*stack)->next->n;
 	pop(stack, line_number);
 	(*stack)->n = sum;
